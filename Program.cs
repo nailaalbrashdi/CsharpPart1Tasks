d472c1fdfeafb00ddf1eff1d146894207915346a
“Naila” <nailaalbrashdi@gmail.com>
2026-01-04 10:00:10 +0400

    adding Task2

--- Program.cs
+++ Program.cs
@@ -7,8 +7,9 @@
                 Console.WriteLine("Welcome to our system");
                 Console.WriteLine("Choose a service you need");
                 Console.WriteLine("1.Find the Average");
-            
+                Console.WriteLine("2.Check if a Number is Even or Odd");
 
+            
             Console.WriteLine("Enter how many numbers you need :");
             int n = int.Parse(Console.ReadLine());
             int[] numbers = new int[n]; //allocating array of size n
@@ -24,6 +25,19 @@
             }
             float average = (float)total / n;
             Console.WriteLine("The average is: " + average);
+
+            
+            Console.WriteLine("Enter a number:");
+            int number1 = int.Parse(Console.ReadLine());
+            if(number1 % 2 == 0)
+        {
+                Console.WriteLine("The number is Even");
+            }
+        else
+            {
+                Console.WriteLine("The number is Odd");
+            }
+
 
 
         }
